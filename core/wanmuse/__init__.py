d968c74fc630c76bf47e42e9f1f6714506d88086
Vlad Jagodnik <blackstoreonline@gmail.com>
2026-07-22 14:50:08 +1200

    Add WanMuse backend package

---
+++ core/wanmuse/__init__.py
@@ -0,0 +1,7 @@
+"""Wan2.2 TI2V-5B + MuseTalk video backend.
+
+Heavy GPU and application imports are intentionally kept out of this package
+initializer so transport/client utilities remain independently testable.
+"""
+
+__all__: list[str] = []
